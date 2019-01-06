b846134b4d6a2ffddaa7005248ebe4d7d18e21bf
Marxsal <throaway@yahoo.com>
2019-01-05 17:53:35 -0800

    More files to add back in.

---
+++ app/src/main/java/com/example/my32ndapplication/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.example.my32ndapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent() ;
+        String message = intent.getStringExtra(TwActivity.EXTRA_MESSAGE);
+
+        // Capture the textview and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
