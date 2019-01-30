9ec2cd36077fdd83fa0f9024aac04011b69d4ca8
Mark <throaway@yahoo.com>
2019-01-30 09:46:22 -0800

    Import basic functionality working

--- app/src/main/java/com/example/my32ndapplication/TwFragment.java
+++ app/src/main/java/com/example/my32ndapplication/TwFragment.java
@@ -3,15 +3,20 @@ package com.example.my32ndapplication;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Debug;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Base64;
 import android.util.JsonReader;
 import android.util.JsonToken;
@@ -20,6 +25,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
@@ -34,19 +40,29 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class TwFragment extends Fragment {
     public static final String LOG_TAG = "32XND-TwFragment";
     public static String TW_FILE_NAME = "com.markqz.tw-fragment-file-name";
 
+    private String mCM;
+    private ValueCallback<Uri> mUM;
+    private ValueCallback<Uri[]> mUMA;
+    private final static int FCR=5;
+    //select whether you want to upload multiple files (set 'true' for yes)
+    private boolean multiple_files = false;
+
     public String tw_file_name;
     WebView webView;
     TwFile twFile ;
     String javascriptEvalResult = "";  // A kluge to get around limits of inner classes
 
-    //private TwViewModel mViewModel;
 
     public static TwFragment newInstance(String sFilename) {
         TwFragment fragment = new TwFragment();
@@ -55,6 +71,55 @@ public class TwFragment extends Fragment {
         fragment.setArguments(bundle);
         return fragment;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(Build.VERSION.SDK_INT >= 21){
+            Uri[] results = null;
+            //checking if response is positive
+            if(resultCode== RESULT_OK){
+                Log.d(LOG_TAG, "onAR - I think result OK");
+                if(requestCode == FCR){
+                    if(null == mUMA){
+                        return;
+                    }
+                    if(intent == null || intent.getData() == null){
+                        if(mCM != null){
+                            results = new Uri[]{Uri.parse(mCM)};
+                        }
+                    }else{
+                        String dataString = intent.getDataString();
+                        if(dataString != null){
+                            results = new Uri[]{Uri.parse(dataString)};
+                        } else {
+                            if(multiple_files) {
+                                if (intent.getClipData() != null) {
+                                    final int numSelectedFiles = intent.getClipData().getItemCount();
+                                    results = new Uri[numSelectedFiles];
+                                    for (int i = 0; i < numSelectedFiles; i++) {
+                                        results[i] = intent.getClipData().getItemAt(i).getUri();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            mUMA.onReceiveValue(results);
+            mUMA = null;
+        }else{
+            if(requestCode == FCR){
+                if(null == mUM) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                mUM.onReceiveValue(result);
+                mUM = null;
+            }
+        }
+    }
+
+
+
 
     @SuppressLint("SetJavaScriptEnabled")
     @Nullable
@@ -73,6 +138,7 @@ public class TwFragment extends Fragment {
         WebSettings webSettings = webView.getSettings() ;
 
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
         final MyAction titleSetter = new MyAction() {
@@ -82,6 +148,8 @@ public class TwFragment extends Fragment {
                 twFile.setTitle(useme);
             }
         } ;
+
+
         webView.setWebViewClient(new WebViewClient() {
 
             @Override
@@ -96,11 +164,107 @@ public class TwFragment extends Fragment {
                          }
         } );
 
-
         //twFile.loadFilePath(); // Need to do this in case content: hasn't been loaded
         String temp = "file:///"+twFile.getUnschemedFilePath() ; // This is probably what we usually call an absolute path
         Log.d(LOG_TAG, "About to load file " + temp);
         webView.loadUrl(temp);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            //handling input[type="file"] requests for android API 16+
+            @SuppressLint("ObsoleteSdkInt")
+            @SuppressWarnings("unused")
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                Log.d(LOG_TAG, "Inside file chooser method 1");
+                mUM = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                if (multiple_files && Build.VERSION.SDK_INT >= 18) {
+                    i.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                }
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FCR);
+            }
+
+            //handling input[type="file"] requests for android API 21+
+            @SuppressLint("InlinedApi")
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                Log.d(LOG_TAG, "Inside file chooser method 2");
+                if (file_permission()) {
+                    Log.d(LOG_TAG, "Passed file permissions check");
+                    String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                    //        Manifest.permission.CAMERA
+                    };
+
+                    //checking for storage permission to write images for upload
+                    if (ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                            && ContextCompat.checkSelfPermission(getActivity(),
+                                    Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                     )
+                    { ActivityCompat.requestPermissions(getActivity(), perms, FCR);
+
+                        //checking for WRITE_EXTERNAL_STORAGE permission
+                    }
+                    else if (ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                    {
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, FCR);
+
+                        //checking for CAMERA permissions
+                    }
+//                    else if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, FCR);
+//                    }
+                    if (mUMA != null) {
+                        mUMA.onReceiveValue(null);
+                    }
+                    mUMA = filePathCallback;
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                            takePictureIntent.putExtra("PhotoPath", mCM);
+                        } catch (IOException ex) {
+                            Log.e(LOG_TAG, "Image file creation failed", ex);
+                        }
+                        if (photoFile != null) {
+                            mCM = "file:" + photoFile.getAbsolutePath();
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                        } else {
+                            takePictureIntent = null;
+                        }
+                    }
+                    Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                    contentSelectionIntent.setType("*/*");
+                    if (multiple_files) {
+                        contentSelectionIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                    }
+                    Intent[] intentArray;
+                    if (takePictureIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent};
+                    } else {
+                        intentArray = new Intent[0];
+                    }
+
+                    Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                    chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                    chooserIntent.putExtra(Intent.EXTRA_TITLE, "File Chooser");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                    startActivityForResult(chooserIntent, FCR);
+                    return true;
+                }else{
+                    Log.d(LOG_TAG, "Did NOT Pass file permissions check");
+                    return false;
+                }
+            }
+        });
+
+
+
 
         //loadTWfromUri(webView, tw_file_name);
 
@@ -113,9 +277,36 @@ public class TwFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+   }
 
-
+    public boolean file_permission(){
+        // 190129 Taking out camera permission for now.
+        if(Build.VERSION.SDK_INT >=23 &&
+                (ContextCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                 //       || ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                ))
+        {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE
+                      //      , Manifest.permission.CAMERA
+                    } , 1);
+            return false;
+        }else{
+            return true;
+        }
     }
+
+    //creating new image file here
+    private File createImageFile() throws IOException{
+        @SuppressLint("SimpleDateFormat")
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_"+timeStamp+"_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName,".jpg",storageDir);
+    }
+
+
 
 
 
